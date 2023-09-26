ea877f0fb8f492af0128f5061628037e2768bd74
AdamArrhaoui <71520592+AdamArrhaoui@users.noreply.github.com>
2023-09-26 16:53:36 -0400

    U-TEST 002: Card value cannot be set to less than 0 or greater than 15.

--- src/test/java/org/example/CardUnitTests.java
+++ src/test/java/org/example/CardUnitTests.java
@@ -19,4 +19,16 @@ class CardUnitTests {
         assertEquals(card.getSuit(), testSuit);
         assertEquals(card.getValue(), testVal);
     }
+    @Test
+    @DisplayName("U-TEST 002: Card value cannot be set to less than 0 or greater than 15.")
+    void testCardValueRangeLimit(){
+        CardType testType = CardType.BASIC;
+        CardSuit testSuit = CardSuit.ARROWS;
+        assertThrows(IllegalArgumentException.class, () -> new Card(testType, testSuit, -1));
+        assertThrows(IllegalArgumentException.class, () -> new Card(testType, testSuit, 16));
+
+        Card card = new Card(testType, testSuit, 1);
+        assertThrows(IllegalArgumentException.class, () -> card.setValue(-1));
+        assertThrows(IllegalArgumentException.class, () -> card.setValue(16));
+    }
 }
