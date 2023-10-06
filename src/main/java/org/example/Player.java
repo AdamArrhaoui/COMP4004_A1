e808ca5e73893494e6fb12233ab324516600cf8d
AdamArrhaoui <adamarrhaoui@cmail.carleton.ca>
2023-10-06 14:28:19 -0400

    CODE 036: (PASS) New player has non-blank name

--- src/main/java/org/example/Player.java
+++ src/main/java/org/example/Player.java
@@ -3,7 +3,12 @@ package org.example;
 public class Player {
     private String name;
 
-    Player(String name){}
+    Player(String name){
+        if (name == null || name.isBlank()){
+            throw new IllegalArgumentException("New player name cannot be null or blank!");
+        }
+        this.name = name;
+    }
 
     public String getName() {
         return name;
