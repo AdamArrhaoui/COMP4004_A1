99ac951565fae831c0edf110b1243e48b1473969
AdamArrhaoui <71520592+AdamArrhaoui@users.noreply.github.com>
2023-10-09 19:31:36 -0400

    CODE 041: (PASS) Player can be prompted to select any card from their hand using the index

--- src/main/java/org/example/Player.java
+++ src/main/java/org/example/Player.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.io.PrintWriter;
 import java.util.Scanner;
+import java.util.StringJoiner;
 
 public class Player {
     private String name;
@@ -29,7 +30,33 @@ public class Player {
         return injuryDeck;
     }
 
-    public Card promptAnyCard(Scanner scanner, PrintWriter printWriter) {
-        return null;
+    public Card promptAnyCard(Scanner input, PrintWriter output) {
+        if (hand.getCards().isEmpty()) return null;
+        // Build string that marks 1-based card indices for the user to choose.
+        StringJoiner stringJoiner = new StringJoiner(" || ", "| ", " |");
+        for (int i = 1; i <= hand.getCards().size(); i++) {
+            // Fancy escape characters to make the numbers in the terminal highlighted purple
+            stringJoiner.add(String.format("\033[32m%-2d\033[0m", i));
+        }
+
+        int selectedIdx = 0;
+        while (selectedIdx == 0){
+            output.println(hand.getCardsString());
+            output.println(stringJoiner.toString());
+            output.print("Select a card index: ");
+            int intInput = 0;
+            try{
+                String inputStr = input.nextLine().strip();
+                intInput = Integer.parseInt(inputStr);
+                if (intInput < 1 || intInput > hand.getCards().size()) throw new IndexOutOfBoundsException();
+            } catch (Exception e) {
+                output.printf("\nInvalid input! Please enter a number between 1 and %d\n", hand.getCards().size());
+                continue;
+            } finally {
+                output.println();
+            }
+            selectedIdx = intInput;
+        }
+        return hand.getCards().get(selectedIdx - 1);
     }
 }
