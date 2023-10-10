2f0108bbc3df03514725d87d7d2e0abfefb9f185
AdamArrhaoui <71520592+AdamArrhaoui@users.noreply.github.com>
2023-10-10 15:54:37 -0400

    CODE 043: (PASS) Player can be prompted to select a valid card suit.

--- src/main/java/org/example/Player.java
+++ src/main/java/org/example/Player.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.io.PrintWriter;
+import java.util.EnumSet;
 import java.util.Scanner;
 import java.util.StringJoiner;
 
@@ -68,6 +69,43 @@ public class Player {
     }
 
     public CardSuit promptCardSuit(Scanner input, PrintWriter output) {
-        return null;
+        // Build and display string with choices of card suits
+        int choiceWidth = 10;
+        //String formatString = "(%d) %-" + choiceWidth + "s";
+        String formatString = "(\033[32m%d\033[0m) %s";
+        StringJoiner promptStringJoiner = new StringJoiner(" | ", "| ", " |");
+        EnumSet<CardSuit> suitOptionSet = EnumSet.complementOf(EnumSet.of(CardSuit.ANY));
+        suitOptionSet.forEach(suit -> promptStringJoiner.add(formatString.formatted(suit.ordinal(), suit.toString())));
+
+        CardSuit selectedSuit = null;
+        while (selectedSuit == null){
+            output.println("Select a suit (type its name or number): ");
+            output.println(promptStringJoiner);
+            output.print("Your choice: ");
+            output.flush();
+
+            String strInput = input.nextLine().replaceAll("\\s", "").toUpperCase();
+
+            // Check for ordinal (index) match
+            try {
+                int index = Integer.parseInt(strInput);
+                if (index > 0 && index < CardSuit.values().length) {  // 0 index is for ANY, which is not a valid choice here.
+                    selectedSuit = CardSuit.values()[index];
+                }
+            } catch (NumberFormatException e) {
+                // Check for name match
+                if (strInput.length() > 1){
+                    for (CardSuit suit : suitOptionSet) {
+                        if (suit.toString().startsWith(strInput)) {
+                            selectedSuit = suit;
+                            break;
+                        }
+                    }
+                }
+            }
+            if (selectedSuit == null)
+                output.println("\nInvalid input! Please enter at least the first 2 characters of the suit's name, or the suit's number\n");
+        }
+        return selectedSuit;
     }
 }
