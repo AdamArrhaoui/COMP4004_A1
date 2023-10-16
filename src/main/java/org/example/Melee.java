978ab29ca50af88d40744dd71b309aa3e6c77484
AdamArrhaoui <adamarrhaoui@cmail.carleton.ca>
2023-10-16 14:04:15 -0400

    CODE 053: (PASS) Melee class created from ordered list of Players, and player as melee leader. Melee leader must be in player list.

--- src/main/java/org/example/Melee.java
+++ src/main/java/org/example/Melee.java
@@ -4,9 +4,15 @@ import java.util.List;
 
 public class Melee {
     private List<Player> players;
-
     private Player playerLeader;
-    Melee(List<Player> players, Player leader){}
+    private int leaderIndex;
+
+    Melee(List<Player> players, Player leader){
+        this.leaderIndex = players.indexOf(leader);
+        if (this.leaderIndex == -1) throw new IllegalArgumentException("Melee leader not in supplied player list!");
+        this.players = players;
+        this.playerLeader = leader;
+    }
 
     public List<Player> getPlayers() {
         return players;
