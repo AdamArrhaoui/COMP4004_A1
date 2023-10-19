76b28478a6cfa445afc115048e63d8f6d8dd6084
AdamArrhaoui <adamarrhaoui@cmail.carleton.ca>
2023-10-19 14:52:15 -0400

    CODE 067: (PASS) TournamentGame class can be created with valid number of players, can be prompted for valid player names for each player.

--- src/main/java/org/example/TournamentGame.java
+++ src/main/java/org/example/TournamentGame.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -11,6 +12,7 @@ public class TournamentGame {
     private List<Player> players;
 
     TournamentGame(int numPlayers){
+        if (numPlayers < MIN_PLAYERS || numPlayers > MAX_PLAYERS) throw new IllegalArgumentException("Player count is invalid!");
         this.numPlayers = numPlayers;
     }
 
@@ -23,5 +25,20 @@ public class TournamentGame {
     }
 
     public void setupPlayers(Scanner input, PrintWriter output) {
+        players = new ArrayList<>(numPlayers);
+        for (int i = 0; i < numPlayers; i++) {
+            String nameStr = "";
+            output.println();
+            while (nameStr == null || nameStr.isBlank()){
+                output.print("\nPlease enter a name for player %d: ".formatted(i+1));
+                output.flush();
+                try {
+                    nameStr = input.nextLine().strip();
+                } catch (Exception e){
+                    output.println("\nInput invalid! Please try again\n");
+                }
+            }
+            players.add(new Player(nameStr));
+        }
     }
 }
