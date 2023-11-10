42134296e87ccdaf7b5bb6be5d221862a3d4e135
AdamArrhaoui <adamarrhaoui@cmail.carleton.ca>
2023-11-10 16:08:36 -0500

    Added playerHasCard step definition
    Gives the player a specific card in their deck.

--- src/test/java/cucumberTests/MeleeStepDefinitions.java
+++ src/test/java/cucumberTests/MeleeStepDefinitions.java
@@ -3,6 +3,7 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.tournamentGame.Card;
 import org.tournamentGame.Melee;
 import org.tournamentGame.Player;
 import org.tournamentGame.TournamentGame;
@@ -10,12 +11,11 @@ import org.tournamentGame.TournamentGame;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class MeleeStepDefinitions {
     private static ArrayList<Player> players;
     private static Melee currentMelee;
-
+    
     @Given("A single melee is started with players named {string}")
     public void SingleMeleePlayers(String playerNames) {
         players = new ArrayList<>(5);
@@ -24,5 +24,18 @@ public class MeleeStepDefinitions {
         }
         assertTrue(players.size() >= TournamentGame.MIN_PLAYERS && players.size() <= TournamentGame.MAX_PLAYERS);
         currentMelee = new Melee(players, players.get(0));
+    }
+
+    @And("{string} has the card {card}")
+    public void playerHasCard(String playerName, Card card) {
+        // Find the desired player
+        Player desiredPlayer = currentMelee.getPlayers().stream()
+                .filter(player -> playerName.equals(player.getName()))
+                .findFirst()
+                .orElse(null);
+        assertNotNull("Can't find player named %s!".formatted(playerName), desiredPlayer);
+        assertNotNull("Can't add null card to %s's hand!".formatted(playerName), card);
+
+        desiredPlayer.getHand().addCard(card);
     }
 }
