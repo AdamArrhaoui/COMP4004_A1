6dc3e299e25c5e3c4999d3ce54bf02865e1b8a53
AdamArrhaoui <adamarrhaoui@cmail.carleton.ca>
2023-11-12 19:55:14 -0500

    Update ParameterTypes.java
    Allowed for more flexibility when converting from string to card. Can now specify cardtype only and not suit to have card with ANY suit

--- src/test/java/cucumberTests/ParameterTypes.java
+++ src/test/java/cucumberTests/ParameterTypes.java
@@ -10,7 +10,13 @@ public class ParameterTypes {
     @ParameterType("(?<Type>[A-Za-z]{2})?(?<Suit>[A-Za-z]{2})(?<Value>\\d{1,2})")
     public Card card(String typeString, String suitString, String valueString) {
         CardType cardType = (typeString != null) ? CardType.fromCode(typeString) : CardType.BASIC;
-        CardSuit cardSuit = CardSuit.fromCode(suitString);
+        CardSuit cardSuit;
+        try {
+            cardSuit = CardSuit.fromCode(suitString);
+        } catch (IllegalArgumentException ignored){
+            cardSuit = CardSuit.ANY;
+            cardType = CardType.fromCode(suitString);
+        }
         int cardValue = Integer.parseInt(valueString);
 
         return new Card(cardType, cardSuit, cardValue);
